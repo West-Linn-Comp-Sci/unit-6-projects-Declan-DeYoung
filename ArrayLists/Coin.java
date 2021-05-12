14f1b2a8ea843410540b2c8fa4382941193b3aa7
Declan-DeYoung <83470597+Declan-DeYoung@users.noreply.github.com>
2021-05-12 14:06:53 -0700

    Update Coin.java

--- ArrayLists/Coin.java
+++ ArrayLists/Coin.java
@@ -1,1 +1,24 @@
 
+public class Coin {  
+  private double myValue; 
+  private String myName; 
+  public Coin(double value, String name) { 
+    myValue = value; 
+    myName = name; 
+  }  
+  public double getValue() { 
+    return myValue; 
+  } 
+  public String getName() { 
+    return myName;
+  }
+  public boolean equals(Coin aCoin) { 
+    String otherName = aCoin.getName();
+    double otherMoney = aCoin.getValue();
+    boolean isSame = false;
+    if(otherMoney = myValue && otherName = myName){
+      isSame = true;
+    }
+    return isSame;
+  }
+}

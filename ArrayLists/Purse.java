c391c1992f9f63c012c1f23031a2ccdac9152db6
Declan-DeYoung <83470597+Declan-DeYoung@users.noreply.github.com>
2021-05-12 18:43:02 -0700

    Update Purse.java

--- ArrayLists/Purse.java
+++ ArrayLists/Purse.java
@@ -1,1 +1,32 @@
 
+public class Purse { 
+  private ArrayList coins; 
+ 
+  public Purse() { 
+    coins = new ArrayList(); 
+  }
+ 
+  public void add(Coin aCoin) { 
+    coins.add(aCoin); 
+  } 
+ 
+// returns total value of all coins in purse 
+  public double getTotal() { 
+    double sum = 0;
+    for(int i = 0; i < coins.size(); i++){
+      Coin whichCoin = coins.get(i);
+      sum += whichCoin.getValue();
+    }
+  } 
+ 
+// returns the number of coins in the Purse that matches aCoin 
+// (both myName & myValue) 
+  public int count(Coin aCoin) {
+    
+   } 
+ 
+// returns the name of the Coin in the Purse that has the smallest value 
+  public String findSmallest(){
+
+  }
+}
